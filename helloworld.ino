cc766f09c353bfabf53fb556c4ca8c20a397cd39
srinji5141 <105142919+srinji5141@users.noreply.github.com>
2023-08-31 21:09:49 -0700

    Update helloworld.ino

--- helloworld.ino
+++ helloworld.ino
@@ -1,4 +1,3 @@
-
 #include <LiquidCrystal.h> // Includes the LiquidCrystal Library 
 LiquidCrystal lcd(3, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7) 
 const int microphonePin = A0;
